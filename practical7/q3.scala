113eb47fd4e378b9da922f26cef9668965e1c5ab
MurshidAkram <murshidakram13@gmail.com>
2024-08-06 06:09:03 +0530

    practical 7

--- practical7/q3.scala
+++ practical7/q3.scala
@@ -1,22 +1,21 @@
-object q3{
+object q3{ 
+  def checkPrime(num: Int): Boolean = {
+    if(num <= 1){
+      return false
+    }
+    if(num == 2){
+      return true
+    }
+   // for(i <- 2 to math.sqrt(num).toInt){
+     // if(num % i == 0){
+       // return false
+     // }
+   // }
+    else!(2 to math.sqrt(num).toInt).exists(i => num % i == 0)
+  }
 
   def filterPrime(list: List[Int]): List[Int] = {
     list.filter(num=>checkPrime(num))
-  } 
-
-  def checkPrime(num: Int): Boolean = {
-    if(num <= 1){
-      false
-    }
-    if(num == 2){
-      true
-    }
-    for(i <- 2 to math.sqrt(num).toInt){
-      if(num % i == 0){
-        false
-      }
-    }
-    true
   }
 
   def main(args: Array[String]): Unit = {
